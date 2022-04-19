08589d5096b4aa406841e562647ac5b53afcc0c2
Americo Meneses <beatamez@hotmail.com>
2022-04-19 17:09:46 -0500

    first commit setting up Project

---
+++ CrudOperationCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrudOperationCoreData
+//
+//  Created by Américo MQ on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
